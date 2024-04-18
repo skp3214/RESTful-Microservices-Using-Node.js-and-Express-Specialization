420c2291935370ab62be55026601641611693725
SACHIN PRAJAPATI <skprajapati3214@gmail.com>
2024-04-19 01:20:03 +0530

    array and it's operations

---
+++ 1-Server side JavaScript with Node.js/Week2/ArrayAndOperations.js
@@ -0,0 +1,12 @@
+console.log('Array Declaration Way');
+
+let car=new Array("Ford","ferrari","mustang");
+console.log(car.length);
+
+let fruits=new Array(3);
+console.log(fruits.length);
+
+let cars=["Ford","Ferrari","Mustang"];
+console.log(cars.length)
+
+// Array Operations 
