9426d0c2c3b459e74ceb34600821e7e0ee7cf54f
SACHIN PRAJAPATI <skprajapati3214@gmail.com>
2024-04-19 01:20:14 +0530

    about arrow function

---
+++ 1-Server side JavaScript with Node.js/Week2/ArrowFunctions.js
@@ -0,0 +1,7 @@
+let display=()=>console.log("Arrow function is used for short cut declaration of anonymous function.");
+
+console.log("arrow function is also used for normal function")
+let sum=function(a,b){return a+b};
+console.log(sum(2,3));
+let calculatesum=(a,b)=>a+b;
+console.log(calculatesum(5,6))
